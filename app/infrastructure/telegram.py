7480fc595fa6198244e6562ab1ca4d1e26acc5de
kolya <so2bahila100@gmail.com>
2026-01-11 22:02:32 +0200

    added detalized logging to function safe_answer

--- app/infrastructure/telegram.py
+++ app/infrastructure/telegram.py
@@ -9,8 +9,8 @@ def init_token(token: str):
 async def safe_answer(message, text: str) -> None:
     try:
         await message.answer(text)
-    except Exception:
-        print("Failed to send message")
+    except Exception as e:
+        print(f"Failed to send message, {message}. The error found: {e}")
 
 ALLOWED_USER_ID = get_or_ask('telegram_user_id', 'Введіть id телеграм користувача: ')
 
