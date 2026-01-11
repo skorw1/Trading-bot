7143554e12c838e912ebf99795504812083940fa
kolya <so2bahila100@gmail.com>
2026-01-11 21:59:39 +0200

    added function safe_asnwer

--- app/infrastructure/telegram.py
+++ app/infrastructure/telegram.py
@@ -2,11 +2,17 @@ from app.infrastructure.config import get_or_ask
 from aiogram import Bot
 bot = None
 
-ALLOWED_USER_ID = get_or_ask('telegram_user_id', 'Введіть id телеграм користувача: ')
-
 def init_token(token: str):
     global bot
     bot = Bot(token=token)
+
+async def safe_answer(message, text: str) -> None:
+    try:
+        await message.answer(text)
+    except Exception:
+        print("Failed to send message")
+
+ALLOWED_USER_ID = get_or_ask('telegram_user_id', 'Введіть id телеграм користувача: ')
 
 TOKEN = get_or_ask('telegram_token', 'Введіть токен телеграм бота: ')
 bot = init_token(TOKEN)
